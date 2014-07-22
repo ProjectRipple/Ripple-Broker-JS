123697d635a78f81b7c6b7cd0bc44d864b9f113b
James West <jwest.126@gmail.com>
2014-07-22 10:32:46 -0400

    Update script to simulate 2 patients

--- test/testudp.py
+++ test/testudp.py
@@ -9,15 +9,28 @@ UDP_PORT = 5690
 # set starting message from hex string(taken from actual mote message)
 source = bytearray.fromhex("d2110012740013b77d5baaaa0000000000000212740013b77d5b0009405e2d0000630000")
 
+source2 = bytearray.fromhex("d2110013740013b15e4baaaa0000000000000213740013b15e4b0009405e2d0000630000")
+
 seq = 0;
 
 while True:
     seq += 1 # increment seq
+
     source[26] = (seq >> 8) & 0xFF
     source[27] = (seq & 0xff)
-    source[28] = random.randint(50,140) # heart rate
-    source[29] = random.randint(85,100) # sp02
+    source[28] = random.randint(70,140) # heart rate
+    source[29] = random.randint(95,100) # sp02
     source[33] = random.randint(90,100) # temperature LSB
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(source, (UDP_IP, UDP_PORT))
-    time.sleep(2)	
+    time.sleep(random.randint(2,4))
+
+    source2[26] = (seq >> 8) & 0xFF
+    source2[27] = (seq & 0xff)
+    source2[28] = random.randint(70,140) # heart rate
+    source2[29] = random.randint(95,100) # sp02
+    source2[33] = random.randint(90,100) # temperature LSB
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(source2, (UDP_IP, UDP_PORT))
+    time.sleep(random.randint(2,4))
+
