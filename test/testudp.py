d8337f080ca5c89f87d681a63a1dc65f37a86697
James West <jwest.126@gmail.com>
2014-08-04 17:22:04 -0400

    Tightened range of vitals in test script & added respiration to give more realistic values

--- test/testudp.py
+++ test/testudp.py
@@ -19,8 +19,9 @@ while True:
     source[26] = (seq >> 8) & 0xFF
     source[27] = (seq & 0xff)
     source[28] = random.randint(70,140) # heart rate
-    source[29] = random.randint(95,100) # sp02
-    source[33] = random.randint(90,100) # temperature LSB
+    source[29] = random.randint(96,100) # sp02
+    source[30] = random.randint(12,20)  # respiration
+    source[33] = random.randint(95,101) # temperature LSB
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(source, (UDP_IP, UDP_PORT))
     time.sleep(random.randint(2,4))
@@ -28,8 +29,9 @@ while True:
     source2[26] = (seq >> 8) & 0xFF
     source2[27] = (seq & 0xff)
     source2[28] = random.randint(70,140) # heart rate
-    source2[29] = random.randint(95,100) # sp02
-    source2[33] = random.randint(90,100) # temperature LSB
+    source2[29] = random.randint(96,100) # sp02
+    source2[30] = random.randint(12,20)  # respiration
+    source2[33] = random.randint(95,101) # temperature LSB
     #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(source2, (UDP_IP, UDP_PORT))
     time.sleep(random.randint(2,4))
